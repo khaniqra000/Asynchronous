74cb76f71496515869dfa6fb784d5add7f490c7f
khaniqra000 <iqrakhan200110@gmail.com>
2024-06-26 23:38:15 +0500

    Assignment of Asynchronous

---
+++ app.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question # 1 
+// Write a simple asynchronous TypeScript function fetchGreeting that returns a 
+// greeting message after a 2-second delay using setTimeout.
+/*console.log("\n\t Question # 1 \n\t");
+
+async function fetchGreeting(){
+    setTimeout(()=>{
+        console.log("Assalamualikum Everyone!");
+    }, 2000 );
+};
+ fetchGreeting();
+
+
+//  Question # 2
+// Write a function simulateTask that simulates a task by logging "Task started",
+// waits for 1 second, and then logs "Task completed". Use setTimeout for the delay.
+console.log("\n\t Question # 2 \n\t");
+
+ function simulateTask(){
+
+     console.log("Task Started");
+
+     setTimeout(()=>{
+        console.log("Task Completed");
+    }, 1000 );
+
+};
+
+simulateTask();
+
+
+// // Question # 3
+// // Write a function fetchData that returns a Promise which resolves with the string
+// // "Data fetched successfully!" after a delay of 1 second.
+ console.log("\n\t Question # 3 \n\t");
+
+ function fetchData(){
+     return new Promise((resolve,reject)=>{
+
+         setTimeout(() => {
+            resolve("Data fetched  successfully!")
+         }, 1000);
+        
+     })
+ }
+
+ fetchData() .then ((Response:any) =>{
+    console.log(Response)
+ });*/
+// Question # 4 
+// Write a function fetchWithError that returns a Promise. It should randomly either 
+// resolve with "Data fetched successfully!" or reject with "Data fetch failed!" after a 
+// delay of 1 second. Handle the rejection using .catch 
+/*console.log("\n\t Question # 4 \n\t");
+
+
+// Question # 5
+// Write a function executeSequentially that executes two functions fetchData and
+// processData sequentially using Promises, and logs the results in the order they are
+// called.
+console.log("\n\t Question # 5 \n\t");*/
+console.log("\n I did not undestand these two question. \n\t I am doing my practice and taking classes . \n Inshallah soon .\n\t I will be completed it and submitted you again");
